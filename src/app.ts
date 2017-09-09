d74ec8a732e1b4bd5232dee7b37e615b8a09ed0d
unknown <kevinkuchar2378@gmail.com>
2017-09-08 21:53:24 -0400

    updates

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,8 @@ import { Scene } from './types'
 
 let store = createStore(reducers);
 
-let scene1: Scene = {
-    id: 1,
-    background: 'test',
-    frames: []
-}
-
-let scene2: Scene = {
-    id: 1,
-    background: 'test',
-    frames: []
-}
-
-store.dispatch(addToScenes(scene1));
-store.dispatch(addToScenes(scene2));
-
-console.log(store.getState());
+// store.dispatch(addToScenes(scene1));
+// store.dispatch(addToScenes(scene2));
 
 class Historia {
     constructor() { }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,28 +1,17 @@
-// export interface State {
-//     scenes: Scene[]
-// }
-
-export interface Position {
-    left: number;
-    right: number;
-    top: number;
-    bottom: number;
-}
-
-export interface Molecule {
-    position: Position;
-    animation: string;
-    text: string
+export interface Scene {
+    id: number
+    background: string;
+    frames: Frame[]
 }
 
 export interface Frame {
     molecules: Molecule[]
 }
 
-export interface Scene {
-    id: number
-    background: string;
-    frames: Frame[]
+export interface Molecule {
+    position: Position;
+    animation: string;
+    text: string
 }
 
 export interface SceneAction {
